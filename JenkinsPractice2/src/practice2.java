cfe18fc4727b899f1ca158ff34c6aabe5a7e0b3b
Pratik0705 <pratik.toke07@gmail.com>
2020-04-07 16:00:11 +0530

    adding for practice of git + jenkins

---
+++ JenkinsPractice2/src/practice2.java
@@ -0,0 +1,12 @@
+
+public class practice2 {
+
+	public static void main(String[] args) {
+		System.out.println("in Jenkins practice 2 ");
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("Jenkins iteration : "+i);
+		}
+	}
+
+}
